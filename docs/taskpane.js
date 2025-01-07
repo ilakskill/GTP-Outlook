e3c9cbdeeff36327e2e7745328b28d5c1f7cb33c
ilakskill <15673241+ilakskill@users.noreply.github.com>
2025-01-06 21:55:05 -0600

    Update taskpane.js

--- docs/taskpane.js
+++ docs/taskpane.js
@@ -16,10 +16,12 @@ Office.onReady(() => {
     function setCookie(name, value, days) {
       const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
       document.cookie = `${name}=${value}; expires=${expires}; path=/; Secure; SameSite=None`;
+      console.log("Setting Cookie: ", document.cookie)
     }
 
     // Helper function to get a cookie
     function getCookie(name) {
+      console.log("Getting Cookie: ", document.cookie)
       const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
       return match ? match[2] : null;
     }
